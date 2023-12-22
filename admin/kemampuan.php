51a972b06ea1ac1f44a73c1e9835c1eddc2ab2dd
QistiIzatus <qistiizatussyabani8910@gmail.com>
2023-12-23 01:34:46 +0700

    menambahkan card pada halaman kemampuan

--- admin/kemampuan.php
+++ admin/kemampuan.php
@@ -5,6 +5,7 @@ require 'layout_header.php';
 $query = 'SELECT * FROM nilai_kemampuan';
 $data = ambildata($conn, $query);
 ?>
+<link rel="stylesheet" href="../style.css">
 <div class="container-fluid">
     <div class="row bg-title">
         <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
@@ -20,6 +21,84 @@ $data = ambildata($conn, $query);
     <!-- /.row -->
     <!-- ============================================================== -->
     <!-- Different data widgets -->
+    
+<div class="grid-container">
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div>
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+    <div class="grid-item">
+        <img src="../assets/img/Haaland.png" alt="Avatar" style="width:100%" >
+    </div>
+</div>
+
+
+
+
+
+
+
+    <!-- <div class="card">
+        <img src="https://thumb.viva.co.id/media/frontend/thumbs3/2023/11/01/6541e20473772-cristiano-ronaldo-meminta-suporter-lawan-tutup-mulut_1265_711.jpg" alt="Avatar" style="width:100%">
+        <div class="container2">
+            <h4><b>John Doe</b></h4> 
+            <p>Architect & Engineer</p> 
+        </div>
+    </div> -->
+
     <a href="./detail_kemampuan.php">Pindah</a>
     <!-- ============================================================== -->
     <!-- .row -->
