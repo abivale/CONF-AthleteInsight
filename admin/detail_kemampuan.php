1104642d5a08ace5c2374a6276c733a445fea025
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2023-12-22 17:41:42 +0700

    menambahkan framework chart.js untuk menampilkan seluruh grafik

--- admin/detail_kemampuan.php
+++ admin/detail_kemampuan.php
@@ -1,8 +1,22 @@
-<div class="flex-container">
-    <img class="img-fluid2" style="flex-grow: 1" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSza4hrtGHwbXEAjTHJRdkM7TLDP8j6Smv29bgYkZuxHbC1O9Hg" alt="">
-    <div class="img-fluid2" style="flex-grow: 1;">
-        <canvas id="myChart"></canvas>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <title>Cristiano Ronaldo</title>
+</head>
+<body>
+    <div class="flex-container">
+        <img class="img-soccer" style="flex-grow: 1" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSza4hrtGHwbXEAjTHJRdkM7TLDP8j6Smv29bgYkZuxHbC1O9Hg" alt="">
+        <div class="img-soccer" style="flex-grow: 1;">
+            <canvas id="myChart"></canvas>
+        </div>
     </div>
-</div>
 
-<script src="../raicab/assets/chart/dataChart.js"></script>
+    <script src="../assets/chart/dataChart.js"></script>
+</body>
+</html>
+
+
